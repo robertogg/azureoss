b2480b7c656498079d073cb7c2842b12af4335d6
Roberto Gonzalez Gomez <robertogg@gmail.com>
2015-08-29 20:06:53 +0200

    Initial

---
+++ obj/Release/Package/PackageTmp/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "db": {
+        "mongodb": "mongodb://admin:admin@ds040888.mongolab.com:40888/MongoLab-t",        
+        "database": "MongoLab-t",
+        "server": "localhost"
+    }
+};
